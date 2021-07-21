5c99659222958d40ce9c686002b29d2fa3162b3f
pswilso2017 <pswilso@us.ibm.com>
2021-07-21 13:31:56 -0500

    Small updates
    Update to bootstrap name in build script.  Added pull secret from my redhat .com login

--- build_script.sh
+++ build_script.sh
@@ -3,7 +3,7 @@
 
  
 
-#qemu-img create -f qcow2 -b /var/lib/libvirt/images/rhcos-qemu.s390x.qcow2 /var/lib/libvirt/images/bootstrap-0.qcow2 100G
+#qemu-img create -f qcow2 -b /var/lib/libvirt/images/rhcos-qemu.s390x.qcow2 /var/lib/libvirt/images/bootstrap.qcow2 100G
 #qemu-img create -f qcow2 -b /var/lib/libvirt/images/rhcos-qemu.s390x.qcow2 /var/lib/libvirt/images/control-0.qcow2 100G
 #qemu-img create -f qcow2 -b /var/lib/libvirt/images/rhcos-qemu.s390x.qcow2 /var/lib/libvirt/images/control-1.qcow2 100G
 #qemu-img create -f qcow2 -b /var/lib/libvirt/images/rhcos-qemu.s390x.qcow2 /var/lib/libvirt/images/control-2.qcow2 100G
