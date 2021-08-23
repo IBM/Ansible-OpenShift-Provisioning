e8a0764999939b186d842679790a944c89c4d860
jacobemery <jacob.emery@ibm.com>
2021-08-23 16:01:56 -0500

    Added two setup scripts for linux and mac

--- ansible-setup-mac.sh
+++ ansible-setup-mac.sh
@@ -4,3 +4,7 @@ ansible-galaxy collection install community.general
 ansible-galaxy collection install community.crypto
 ansible-galaxy collection install ansible.posix
 ansible-galaxy collection install community.libvirt
+
+brew install sshpass -y
+brew install openssh -y
+brew install ssh-copy-id -y
