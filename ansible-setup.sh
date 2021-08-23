985550a37a5dc0f274756914b3b784546964a8c8
jacobemery <jacob.emery@ibm.com>
2021-08-23 15:20:00 -0500

    Adding libvirt to ansible-setup shell script

--- ansible-setup.sh
+++ ansible-setup.sh
@@ -3,3 +3,4 @@
 ansible-galaxy collection install community.general
 ansible-galaxy collection install community.crypto
 ansible-galaxy collection install ansible.posix
+ansible-galaxy collection install community.libvirt
