cb6176b87053b13fcea419a9cc46c1b155a988f4
jacobemery <jacob.emery@ibm.com>
2021-08-23 16:25:39 -0500

    Took out path from copy-id task

--- ansible-setup-linux.sh
+++ ansible-setup-linux.sh
@@ -6,4 +6,3 @@ ansible-galaxy collection install ansible.posix
 ansible-galaxy collection install community.libvirt
 sudo dnf install sshpass -y
 sudo dnf install openssh -y
-sudo dnf install ssh-copy-id -y
