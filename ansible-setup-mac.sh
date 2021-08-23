d0f56c249a09369c71184d57da4ea289f465d17f
jacobemery <jacob.emery@ibm.com>
2021-08-23 16:07:38 -0500

    Adding sudo to shell scripts

--- ansible-setup-mac.sh
+++ ansible-setup-mac.sh
@@ -4,7 +4,6 @@ ansible-galaxy collection install community.general
 ansible-galaxy collection install community.crypto
 ansible-galaxy collection install ansible.posix
 ansible-galaxy collection install community.libvirt
-
-brew install sshpass -y
-brew install openssh -y
-brew install ssh-copy-id -y
+sudo brew install sshpass -y
+sudo brew install openssh -y
+sudo brew install ssh-copy-id -y
