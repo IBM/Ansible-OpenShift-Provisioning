a200204680867bbe712bce390d4350d159805dcd
jacobemery <jacob.emery@ibm.com>
2021-09-01 11:06:08 -0500

    Deleted deprecated shell scripts, started work on implementing Ansible Vault encryption during setup, created new ansible-setup scripts for linux and mac, updated README.

--- files/shell_scripts/prep_kvm_guests.sh
+++
@@ -1,4 +0,0 @@
-#!bin/bash
-wget https://mirror.openshift.com/pub/openshift-v4/s390x/dependencies/rhcos/latest/latest/rhcos-qemu.s390x.qcow2.gz
-dnf install -y gzip
-gunzip rhcos-qemu.s390x.qcow2.gz /var/lib/libvirt/images/
