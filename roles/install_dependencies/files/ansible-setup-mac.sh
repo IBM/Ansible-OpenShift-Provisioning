c78a6b8c947bd0c62a6f3aac833563039f3071d5
jacobemery <jacob.emery@ibm.com>
2021-09-03 12:59:26 -0500

    removed setup scripts and incorporated them into install dependencies' main playbook for simplicity

--- roles/install_dependencies/files/ansible-setup-mac.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-ansible-galaxy collection install community.general
-ansible-galaxy collection install community.crypto
-ansible-galaxy collection install ansible.posix
-ansible-galaxy collection install community.libvirt
-brew install openssh
