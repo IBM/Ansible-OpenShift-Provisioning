e40034d3a743bba792fb64900c46e96eda06d463
jacobemery <jacob.emery@ibm.com>
2021-09-03 12:59:26 -0500

    removed setup scripts and incorporated them into install dependencies' main playbook for simplicity

--- roles/install_dependencies/files/ansible-setup-linux.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-ansible-galaxy collection install community.general
-ansible-galaxy collection install community.crypto
-ansible-galaxy collection install ansible.posix
-ansible-galaxy collection install community.libvirt
-sudo dnf install sshpass -y
-sudo dnf install openssh -y
