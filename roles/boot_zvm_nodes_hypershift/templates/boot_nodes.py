5de4574aaff6409839f678144a8869905d4eced9
root <root@m42lp53.lnxero1.boe>
2024-02-06 16:10:39 +0100

    Updated boot_nodes.py with osa-fix
    Signed-off-by: root <root@m42lp53.lnxero1.boe>

--- roles/boot_zvm_nodes_hypershift/templates/boot_nodes.py
+++ roles/boot_zvm_nodes_hypershift/templates/boot_nodes.py
@@ -23,7 +23,7 @@ parameters = {
 
 interfaces=[]
 if args.network.lower() == 'osa':
-    interfaces=[{ "type": "osa", "id": "{{ hypershift.agents_parms.zvm_parameters.nodes[item].interface.id.split(',') | map('regex_replace', '0.0.', ' ,') | join(' ') | replace(' ', '') | replace_first(',', '') }}"}]
+    interfaces=[{ "type": "osa", "id": "{{ hypershift.agents_parms.zvm_parameters.nodes[item].interface.id.split(',') | map('regex_replace', '0.0.', '') | join(',') }}"}]
 
 guest_parameters = {
 "boot_method": "network",
